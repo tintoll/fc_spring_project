d5e10a217e21ce519940c7680fbd3809ee934ebd
tintoll <belselios@gmail.com>
2019-07-20 07:07:41 +0900

    springbasic refactoring and lombok add

--- springbasic/src/main/java/com/study/Main.java
+++ springbasic/src/main/java/com/study/Main.java
@@ -9,20 +9,19 @@ public class Main {
 
     private static Logger logger = LoggerFactory.getLogger(Main.class);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
         logger.info("hello world!");
 
         // JDBC 사용법
-        Connection connection = null;
-        Statement statement = null;
-        try {
-            Class.forName("org.h2.Driver");
-            // ~/test 파일로 연결하고 MySQL호환모드를 사용하겠다는 의미
-            // String url = "jdbc:h2~/test;MODE=MySQL";
+        Class.forName("org.h2.Driver");
+        // ~/test 파일로 연결하고 MySQL호환모드를 사용하겠다는 의미
+        // String url = "jdbc:h2~/test;MODE=MySQL";
+        String url = "jdbc:h2:mem:test;MODE=MySQL"; // 메모리로 연결
 
-            String url = "jdbc:h2:mem:test;MODE=MySQL"; // 메모리로 연결
-            connection = DriverManager.getConnection(url, "sa", "");
-            statement = connection.createStatement();
+        // java7에나 나온  try~resource 문법 Autoclose 자동으로 된다.
+        // Connection 과 Statement 는 AutoCloseable 상속받고 있어서 가능함.
+        try ( Connection connection = DriverManager.getConnection(url, "sa", "");
+              Statement statement = connection.createStatement()){
 
             // 트랜잭션 확인을 위해
             connection.setAutoCommit(false);
@@ -30,42 +29,23 @@ public class Main {
             // 테이블 생성
             statement.execute("create table member(id int auto_increment, username varchar(255) not null, password varchar(255) not null, primary key(id))");
 
-            // 데이터 추가
-            statement.executeUpdate("insert into member(username, password) values('test', '1234')");
+            try {
+                // 데이터 추가
+                statement.executeUpdate("insert into member(username, password) values('test', '1234')");
+                connection.commit();
+            } catch(SQLException e) {
+                connection.rollback();
+            }
 
             ResultSet resultSet = statement.executeQuery("select id, username, password from member");
             while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String username = resultSet.getString("username");
-                String password = resultSet.getString("password");
-
-                logger.info("id : "+id+", username : "+username+", password : "+password);
+                Member member = new Member(resultSet);
+                logger.info(member.toString());
             }
 
-            connection.commit();
 
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        } catch (SQLException e) {
-            e.printStackTrace();
-
-            // 롤백 
-            try {
-                connection.rollback();
-            } catch (SQLException e1) {
-                e1.printStackTrace();
-            }
-        } finally {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+        }  catch (SQLException e) {
+           e.printStackTrace();
         }
 
     }
---
+++ springbasic/src/main/java/com/study/Member.java
@@ -0,0 +1,30 @@
+package com.study;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Member {
+    private int id;
+    @NonNull private String username;
+    @NonNull private String password;
+
+    public Member(ResultSet resultSet) {
+        try {
+            this.id = resultSet.getInt("id");
+            this.username = resultSet.getString("username");
+            this.password = resultSet.getString("password");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
