6428c860a939eef0b66cd31cdf4f6994c985fc13
tintoll <belselios@gmail.com>
2019-07-27 16:33:26 +0900

    eatgo 가게상세 api 추가

--- eatgo/eatgo-api/src/main/java/io/tintoll/eatgo/interfaces/RestauranController.java
+++ eatgo/eatgo-api/src/main/java/io/tintoll/eatgo/interfaces/RestauranController.java
@@ -2,6 +2,7 @@ package io.tintoll.eatgo.interfaces;
 
 import io.tintoll.eatgo.domain.Restaurant;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -13,9 +14,22 @@ public class RestauranController {
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
         List<Restaurant> restaurants = new ArrayList<>();
-        Restaurant restaurant = new Restaurant(1004L,"Bob zip","Seoul");
-        restaurants.add(restaurant);
+        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
 
         return restaurants;
+    }
+
+    @GetMapping("/restaurants/{id}")
+    public Restaurant detail(@PathVariable("id") Long id) {
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+        restaurants.add(new Restaurant(2020L,"Cyber food","Seoul"));
+
+        Restaurant restaurant = restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null); // 없으면 null을 리턴
+                // .get();
+        return restaurant;
     }
 }
