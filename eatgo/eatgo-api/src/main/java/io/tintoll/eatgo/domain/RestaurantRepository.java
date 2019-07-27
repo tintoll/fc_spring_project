e161a4077e10b836150fc7578cdb34e14561e3c2
tintoll <belselios@gmail.com>
2019-07-27 16:41:26 +0900

    eatgo Repository로 리팩토링

---
+++ eatgo/eatgo-api/src/main/java/io/tintoll/eatgo/domain/RestaurantRepository.java
@@ -0,0 +1,26 @@
+package io.tintoll.eatgo.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantRepository {
+
+    private List<Restaurant> restaurants = new ArrayList<>();
+
+    public RestaurantRepository() {
+        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
+        restaurants.add(new Restaurant(2020L,"Cyber food","Seoul"));
+    }
+
+    public List<Restaurant> findAll() {
+        return restaurants;
+    }
+
+    public Restaurant findById(Long id) {
+        return restaurants.stream()
+                .filter(r -> r.getId().equals(id))
+                .findFirst()
+                .orElse(null); // 없으면 null을 리턴
+                // .get();
+    }
+}
--- eatgo/eatgo-api/src/main/java/io/tintoll/eatgo/interfaces/RestauranController.java
+++ eatgo/eatgo-api/src/main/java/io/tintoll/eatgo/interfaces/RestauranController.java
@@ -1,35 +1,25 @@
 package io.tintoll.eatgo.interfaces;
 
 import io.tintoll.eatgo.domain.Restaurant;
+import io.tintoll.eatgo.domain.RestaurantRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class RestauranController {
 
+    private RestaurantRepository repository = new RestaurantRepository();
+
     @GetMapping("/restaurants")
     public List<Restaurant> list() {
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
-
-        return restaurants;
+     return repository.findAll();
     }
 
     @GetMapping("/restaurants/{id}")
     public Restaurant detail(@PathVariable("id") Long id) {
-        List<Restaurant> restaurants = new ArrayList<>();
-        restaurants.add(new Restaurant(1004L,"Bob zip","Seoul"));
-        restaurants.add(new Restaurant(2020L,"Cyber food","Seoul"));
-
-        Restaurant restaurant = restaurants.stream()
-                .filter(r -> r.getId().equals(id))
-                .findFirst()
-                .orElse(null); // 없으면 null을 리턴
-                // .get();
-        return restaurant;
+        return repository.findById(id);
     }
 }
