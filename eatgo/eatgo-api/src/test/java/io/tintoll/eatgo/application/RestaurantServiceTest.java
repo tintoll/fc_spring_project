3cf19d42dc801afff46b444220b7feb1421b13f6
tintoll <belselios@gmail.com>
2019-07-31 06:39:41 +0900

    eatgo fake object using test

--- eatgo/eatgo-api/src/test/java/io/tintoll/eatgo/application/RestaurantServiceTest.java
+++ eatgo/eatgo-api/src/test/java/io/tintoll/eatgo/application/RestaurantServiceTest.java
@@ -3,26 +3,53 @@ package io.tintoll.eatgo.application;
 import io.tintoll.eatgo.domain.*;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
 
 public class RestaurantServiceTest {
 
     private RestaurantService restaurantService;
+
+    @Mock
     private RestaurantRepository restaurantRepository;
+    @Mock
     private MenuItemRepository menuItemRepository;
 
     // 모든 테스트를 실행전에 먼저 실행해주는 부분이다.
     @Before
     public void setUp() {
         // 현재 Spring 테스트가 아니기 때문에 의존관계를 생성해줘야 한다.
-        // 서비스는 spring에 상관없이 해당 로직에만 집중하여 작업을 하면 된다. 
-        restaurantRepository = new RestaurantRepositoryImpl();
-        menuItemRepository = new MenuItemRepositoryImpl();
+        // 서비스는 spring에 상관없이 해당 로직에만 집중하여 작업을 하면 된다.
+//        restaurantRepository = new RestaurantRepositoryImpl();
+//        menuItemRepository = new MenuItemRepositoryImpl();
+
+        // @Mock를 초기화해준다.
+        MockitoAnnotations.initMocks(this);
+        mockRestaurantRepository();
+        mockMenuItemRepository();
+
         restaurantService = new RestaurantService(restaurantRepository, menuItemRepository);
+    }
+
+    private void mockRestaurantRepository() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        Restaurant restaurant = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurants.add(restaurant);
+
+        given(restaurantRepository.findAll()).willReturn(restaurants);
+        given(restaurantRepository.findById(1004L)).willReturn(restaurant);
+    }
+    private void mockMenuItemRepository() {
+        List<MenuItem> menuItems = new ArrayList<>();
+        menuItems.add(new MenuItem("Kimchi"));
+        given(menuItemRepository.findAllByRestaurantId(1004L)).willReturn(menuItems);
     }
 
     @Test
--- eatgo/eatgo-api/src/test/java/io/tintoll/eatgo/interfaces/RestauranControllerTest.java
+++ eatgo/eatgo-api/src/test/java/io/tintoll/eatgo/interfaces/RestauranControllerTest.java
@@ -1,19 +1,21 @@
 package io.tintoll.eatgo.interfaces;
 
 import io.tintoll.eatgo.application.RestaurantService;
-import io.tintoll.eatgo.domain.MenuItemRepository;
-import io.tintoll.eatgo.domain.MenuItemRepositoryImpl;
-import io.tintoll.eatgo.domain.RestaurantRepository;
-import io.tintoll.eatgo.domain.RestaurantRepositoryImpl;
+import io.tintoll.eatgo.domain.*;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.hamcrest.Matchers.*;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,17 +28,25 @@ public class RestauranControllerTest {
     private MockMvc mvc;
 
     // RestauranController에서 사용하는 객체의 의존성을 주입해주어야 한다.
-    @SpyBean(RestaurantRepositoryImpl.class) // 실제 구현체를 넣어줘야 에러가 나지 않는다.
-    private RestaurantRepository restaurantRepository;
+    // 실제 구현체를 넣어줘야 에러가 나지 않는다.
+//    @SpyBean(RestaurantRepositoryImpl.class)
+//    private RestaurantRepository restaurantRepository;
+//
+//    @SpyBean(MenuItemRepositoryImpl.class)
+//    private MenuItemRepository menuItemRepository;
 
-    @SpyBean(MenuItemRepositoryImpl.class)
-    private MenuItemRepository menuItemRepository;
-
-    @SpyBean(RestaurantService.class)
+    // 가짜 객체를 넣어준다.
+    @MockBean
     private RestaurantService restaurantService;
 
     @Test
     public void list() throws Exception {
+        // 컨트롤러는 서비스가 어떻게 동작하는지 관심이 없고 활용하는 부분에 의의를 둠.
+        // 가짜 데이터를 넣어서 확인을 하여준다.
+        List<Restaurant> restaurants = new ArrayList<>();
+        restaurants.add(new Restaurant(1004L, "Bob zip", "Seoul"));
+        given(restaurantService.getRestaurants()).willReturn(restaurants);
+
         mvc.perform(get("/restaurants"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
@@ -49,6 +59,14 @@ public class RestauranControllerTest {
 
     @Test
     public void detail() throws Exception {
+        Restaurant restaurant1 = new Restaurant(1004L, "Bob zip", "Seoul");
+        restaurant1.addMenuItem(new MenuItem("Kimchi"));
+        Restaurant restaurant2 = new Restaurant(2020L, "Cyber food", "Seoul");
+
+        given(restaurantService.getRestaurantById(1004L)).willReturn(restaurant1);
+        given(restaurantService.getRestaurantById(2020L)).willReturn(restaurant2);
+
+
         mvc.perform(get("/restaurants/1004"))
                 .andExpect(status().isOk())
                 .andExpect(content().string(
