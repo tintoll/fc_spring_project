62c715cec6dcd879aacf5ae286300a526a69e4c5
tintoll <belselios@gmail.com>
2019-08-17 07:20:04 +0900

    admin user entity and repository add

---
+++ admin/src/main/java/com/tintoll/admin/model/entity/User.java
@@ -0,0 +1,36 @@
+package com.tintoll.admin.model.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String account;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private LocalDateTime createdAt;
+
+    private String createdBy;
+
+    private LocalDateTime updatedAt;
+
+    private String updatedBy;
+}
