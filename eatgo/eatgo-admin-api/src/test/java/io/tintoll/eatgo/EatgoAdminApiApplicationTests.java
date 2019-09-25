21f7fc200d8f7af1bba35bc84d39037b6274a513
tintoll <belselios@gmail.com>
2019-09-25 15:31:02 +0900

    eatgo project detaching 1

--- eatgo/eatgo-admin-api/src/test/java/io/tintoll/eatgo/EatgoAdminApiApplicationTests.java
+++ eatgo/eatgo-admin-api/src/test/java/io/tintoll/eatgo/EatgoAdminApiApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class EatgoApplicationTests {
+public class EatgoAdminApiApplicationTests {
 
 	@Test
 	public void contextLoads() {
