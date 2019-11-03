bce9304658d396aa76dd20a3d6b2047a62b3a10d
tintoll <belselios@gmail.com>
2019-11-04 07:27:12 +0900

    admin 요청/응답 dto 작성

---
+++ admin/src/main/java/com/tintoll/admin/controller/api/UserApiController.java
@@ -0,0 +1,8 @@
+package com.tintoll.admin.controller.api;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserApiController {
+
+}
---
+++ admin/src/main/java/com/tintoll/admin/ifs/CrudInterface.java
@@ -0,0 +1,5 @@
+package com.tintoll.admin.ifs;
+
+public interface CrudInterface {
+
+}
