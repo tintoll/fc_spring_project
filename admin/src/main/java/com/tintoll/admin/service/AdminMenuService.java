2c057f8ff4b3cae4341dd297027c11fbfe6eede9
tintoll <belselios@gmail.com>
2019-11-13 08:26:26 +0900

    admin front 소스 추가

---
+++ admin/src/main/java/com/tintoll/admin/service/AdminMenuService.java
@@ -0,0 +1,21 @@
+package com.tintoll.admin.service;
+
+
+import com.tintoll.admin.model.front.AdminMenu;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class AdminMenuService {
+
+    public List<AdminMenu> getAdminMenu(){
+
+        return Arrays.asList(
+                AdminMenu.builder().title("고객 관리").url("/pages/user").code("user").build()
+        );
+
+    }
+
+}
